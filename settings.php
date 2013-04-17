1233d189d74313935a3e230ccad4700f3b53110b
ps3com <p.sharples@bolton.ac.uk>
2013-04-17 18:15:19 +0100

    settings file

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+$settings->add(new admin_setting_configtext('mashup_wookie_url','Wookie Server URL',
+                  '', "http://localhost:8080/wookie/", PARAM_TEXT));
+
+$settings->add(new admin_setting_configtext('mashup_wookie_key', 'Wookie API Key',
+                   '', "TEST", PARAM_TEXT));
+
+$settings->add(new admin_setting_configtextarea('mashup_wookie_moodleparams','Moodle Params',
+					get_string('moodleparamshelp','format_mashup'),'',PARAM_RAW));
+
+?>
