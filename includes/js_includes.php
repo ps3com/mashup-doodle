7ed4884f05cb6771a7520412b10db0eea23cff80
ps3com <p.sharples@bolton.ac.uk>
2013-04-18 16:15:31 +0100

    more moodle course files (some based on weeks format)

---
+++ includes/js_includes.php
@@ -0,0 +1,21 @@
+<?php
+echo PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/gridster/jquery.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery-ui-1.9.2.custom.min.js"></script>'.PHP_EOL;
+
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.core.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.widget.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.position.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.button.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.menu.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.menubar.js"></script>'.PHP_EOL;
+
+echo '<script type="text/javascript" src="/course/format/mashup/script/gridster/jquery.gridster-forked.js"></script>'.PHP_EOL;
+
+echo '<script type="text/javascript" src="/course/format/mashup/script/contextmenu/jquery.contextMenu.js"></script>'.PHP_EOL;
+
+echo '<script type="text/javascript" src="/course/format/mashup/script/json/json2.js"></script>'.PHP_EOL;
+
+echo '<script type="text/javascript" src="/course/format/mashup/script/mashup_properties.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/mashupengine.js"></script>'.PHP_EOL;
+?>
---
+++ version.php
@@ -0,0 +1,6 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2012112900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2012112900;        // Requires this Moodle version.
+$plugin->component = 'format_mashup';    // Full name of the plugin (used for diagnostics).
