2f422e9cb29c66124535341230e544dd7e1196c9
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:17:11 +0100

    Added the <app> positioning code

--- classes/omdl/OmdlInputAdapter.php
+++ classes/omdl/OmdlInputAdapter.php
@@ -29,9 +29,68 @@ class OmdlInputAdapter {
 	// unknown region (possibly a 4th region)
 	private $unknown = array();
 	
+	public function getAllUrls(){
+		$newList = array();
+		$newList  = array_merge($this->getAllLeftUrls(), $this->getAllCenterUrls(), $this->getAllRightUrls(), $this->getAllUnknownUrls());
+		return $newList;
+	}
+	
+	public function getAllUnknownUrls(){
+		return array_reverse($this->unknown);
+	}
+	
+	public function getAllRightUrls(){
+		$newList = array();
+		$newList = array_merge(array_reverse($this->topRight), array_reverse($this->middleRight), array_reverse($this->bottomRight));
+		return $newList;
+	}
+	
+	public function getAllCenterUrls(){
+		$newList = array();
+		$newList = array_merge(array_reverse($this->topCenter), array_reverse($this->middleCenter), array_reverse($this->bottomCenter));
+		return $newList;
+	}
+	
+	public function getAllLeftUrls(){		
+		$newList = array();
+		$newList = array_merge(array_reverse($this->topLeft), array_reverse($this->middleLeft), array_reverse($this->bottomLeft));
+		return $newList;
+	}
 	
 	public function addToAppMap($widgetReference, $position){
-		//TODO
+		if(strpos($position, OmdlConstants::POSITION_LEFT)!== false){
+            $LEFT_FOUND = true;
+            if(strpos($position, OmdlConstants::POSITION_TOP)!== false){
+            	array_push($this->topLeft, $widgetReference);
+            }else if (strpos($position, OmdlConstants::POSITION_MIDDLE)!== false){
+                array_push($this->middleLeft, $widgetReference);
+            }else{// otherwise go to bottom
+                array_push($this->bottomLeft, $widgetReference);
+            }
+        }
+        else if(strpos($position, OmdlConstants::POSITION_CENTER)!== false){
+            $CENTER_FOUND = true;
+            if(strpos($position, OmdlConstants::POSITION_TOP)!== false){	
+                array_push($this->topCenter, $widgetReference);
+            }else if (strpos($position, OmdlConstants::POSITION_MIDDLE)!== false){
+                array_push($this->middleCenter, $widgetReference);
+            }else{// otherwise go to bottom
+                array_push($this->bottomCenter, $widgetReference);
+            }
+        }
+        else if(strpos($position, OmdlConstants::POSITION_RIGHT)!== false){
+            $RIGHT_FOUND = true;
+            if(strpos($position, OmdlConstants::POSITION_TOP)!== false){
+            	array_push($this->topRight, $widgetReference);
+            }else if (strpos($position, OmdlConstants::POSITION_MIDDLE)!== false){
+                array_push($this->middleRight, $widgetReference);
+            }else{// otherwise go to bottom
+                array_push($this->bottomRight, $widgetReference);
+            }
+        }
+        else{
+            $this->unknown.add(widgetReference);
+        }
 	}
 	
 	public function getName() {
