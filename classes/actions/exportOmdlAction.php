dfa6a45d44fc7eef90828b79ade0e3d596520f4d
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:09:52 +0100

    added user check

--- classes/actions/exportOmdlAction.php
+++ classes/actions/exportOmdlAction.php
@@ -7,20 +7,26 @@ require_login($id);
 
 $PAGE->set_url('/course/format/mashup/classes/actions/exportOmdlAction.php', array('id' => $id));
 
-//TODO add in the has_capability() check so only course_creator/teacher can do this
-
-if (isset($_GET['courseId']) && isset($_GET['pageId'])){
-	$courseId = $_GET['courseId'];
-	$pageId = $_GET['pageId'];
-	$omdlExporter = new OMDLExporter($courseId, $pageId);
-	// Send the headers
-
-	header('Content-type: text/xml');
-	header('Pragma: public');
-	header('Cache-control: private');
-	header('Expires: -1');
-	header('Content-Disposition: attachment; filename="moodle-mashup-page-'.$pageId.'.xml"');
+// set permissions to prevent students etc to execute this
+$course = $PAGE->course;
+$context = get_context_instance(CONTEXT_COURSE, $course->id);
+if (!has_capability('moodle/course:manageactivities', $context)) {
+	echo "You do not have permission to complete this action";
+}
+else{
+	if (isset($_GET['courseId']) && isset($_GET['pageId'])){
+		$courseId = $_GET['courseId'];
+		$pageId = $_GET['pageId'];
+		$omdlExporter = new OMDLExporter($courseId, $pageId);
+		// Send the headers
 	
-	echo $omdlExporter->toXml();
-}		
+		header('Content-type: text/xml');
+		header('Pragma: public');
+		header('Cache-control: private');
+		header('Expires: -1');
+		header('Content-Disposition: attachment; filename="moodle-mashup-page-'.$pageId.'.xml"');
+		
+		echo $omdlExporter->toXml();
+	}	
+}
 ?>
