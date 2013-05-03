ce78a47924c8c32a565a7558381097bee8347517
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 12:54:56 +0100

    updated settings to include wookie admin details

--- settings.php
+++ settings.php
@@ -5,6 +5,12 @@ $settings->add(new admin_setting_configtext('mashup_wookie_url','Wookie Server U
 $settings->add(new admin_setting_configtext('mashup_wookie_key', 'Wookie API Key',
                    '', "TEST", PARAM_TEXT));
 
+$settings->add(new admin_setting_configtext('mashup_wookie_admin_username', 'Wookie Admin username',
+		'', "java", PARAM_TEXT));
+
+$settings->add(new admin_setting_configtext('mashup_wookie_admin_password', 'Wookie Admin password',
+		'', "java", PARAM_TEXT));
+
 $settings->add(new admin_setting_configtextarea('mashup_wookie_moodleparams','Moodle Params',
 					get_string('moodleparamshelp','format_mashup'),'',PARAM_RAW));
 
