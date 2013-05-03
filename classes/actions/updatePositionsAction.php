7d3b91a7f999150b3dbf4c4bfbe4ddb84e313d71
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:05:14 +0100

    added check to make sure user is the teacher/course co-ordinator

--- classes/actions/updatePositionsAction.php
+++ classes/actions/updatePositionsAction.php
@@ -5,20 +5,27 @@ $id = required_param('courseId', PARAM_INT);
 require_login($id);
 $PAGE->set_url('/course/format/mashup/classes/actions/updatePositionsAction.php', array('id' => $id));
 
+//global $USER, $CFG, $DB;
 
-//TODO add in the has_capability() check so only course_creator/teacher can do this
-
-if (isset($_POST['courseId']) && isset($_POST['dataEnv'])){
-	$mashupDatabaseHelper = new MashupDatabaseHelper();
-	$result="";
-	$courseId = $_POST['courseId'];
-	$json = $_POST['dataEnv'];
-	$gridsterWidgets = json_decode($json);
-	foreach ($gridsterWidgets as $gridsterWidget){
-		if(!$mashupDatabaseHelper->updateWidget($gridsterWidget, $courseId)){
-			$result.= $mashupDatabaseHelper->get_error() ."\n";
+// set permissions to prevent students etc to execute this
+$course = $PAGE->course;
+$context = get_context_instance(CONTEXT_COURSE, $course->id);
+if (!has_capability('moodle/course:manageactivities', $context)) {
+	echo "You do not have permission to complete this action";
+}
+else{
+	if (isset($_POST['courseId']) && isset($_POST['dataEnv'])){
+		$mashupDatabaseHelper = new MashupDatabaseHelper();
+		$result="";
+		$courseId = $_POST['courseId'];
+		$json = $_POST['dataEnv'];
+		$gridsterWidgets = json_decode($json);
+		foreach ($gridsterWidgets as $gridsterWidget){
+			if(!$mashupDatabaseHelper->updateWidget($gridsterWidget, $courseId)){
+				$result.= $mashupDatabaseHelper->get_error() ."\n";
+			}
 		}
+		echo $result;
 	}
-	echo $result;
 }		
 ?>
