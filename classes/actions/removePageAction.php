a1d911c6692dbdb8629ae6c9ebe9e5483f72c5ea
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:06:21 +0100

    added more checks to make sure user is the teacher/course co-ordinator

--- classes/actions/removePageAction.php
+++ classes/actions/removePageAction.php
@@ -5,15 +5,20 @@ $id = required_param('courseId', PARAM_INT);
 require_login($id);
 $PAGE->set_url('/course/format/mashup/classes/actions/removePageAction.php', array('id' => $id));
 
-
-//TODO add in the has_capability() check so only course_creator/teacher can do this
-
-if (isset($_POST['courseId']) && isset($_POST['pageId'])){
-	$mashupDatabaseHelper = new MashupDatabaseHelper();
-	$result="";
-	$courseId = $_POST['courseId'];
-	$pageId = $_POST['pageId'];
-	$result.=$mashupDatabaseHelper->deletePage($pageId);
-	echo $result;
+// set permissions to prevent students etc to execute this
+$course = $PAGE->course;
+$context = get_context_instance(CONTEXT_COURSE, $course->id);
+if (!has_capability('moodle/course:manageactivities', $context)) {
+	echo "You do not have permission to complete this action";
+}
+else{
+	if (isset($_POST['courseId']) && isset($_POST['pageId'])){
+		$mashupDatabaseHelper = new MashupDatabaseHelper();
+		$result="";
+		$courseId = $_POST['courseId'];
+		$pageId = $_POST['pageId'];
+		$result.=$mashupDatabaseHelper->deletePage($pageId);
+		echo $result;
+	}
 }		
 ?>
