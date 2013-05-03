e0b993c903e7085c11058afd34168dbd7ef44a81
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:01:12 +0100

    removed the HTML generation (now part of the js api)

--- classes/InstanceGenerator.php
+++ classes/InstanceGenerator.php
@@ -13,51 +13,18 @@ class InstanceGenerator {
 		$this->instanceId = $instanceId;
 		$this->connection = new WookieConnectorService ($CFG->mashup_wookie_url, $CFG->mashup_wookie_key, $this->instanceId, $USER->id, $USER->username);
 	}
-
-	public function getInstanceMarkup($persistedWidget){
-		global $USER;		
-		$widgetInstance = $this->getWidget($this->connection, $persistedWidget->getUrl());
-		
+	
+	public function getWidgetInstance($persistedWidget){
+		global $USER;
+		$widgetInstance = $this->getWidget($this->connection, $persistedWidget->getUrl());	
 		//set username
 		$this->setProperty($widgetInstance, "username", $USER->username, $this->connection);
-		// Add participant
-		//$this->addParticipant($widgetInstance, $conn);
-		$layout="";
-		$layout.=$this->generateMarkupForWidget($widgetInstance, $persistedWidget);
-		return $layout;
-	}
-
-	private function generateMarkupForWidget($widgetInstance, $persistedWidget){
-		$output="";
-		$output.='<li id="widget-li-'.$persistedWidget->getId().'" data-localident="'.$persistedWidget->getId().'" data-row="'.$persistedWidget->getDataRow().'" data-col="'.$persistedWidget->getDataCol().'" data-sizex="'.$persistedWidget->getDataSizeX().'" data-sizey="'.$persistedWidget->getDataSizeY().'">'.PHP_EOL;
-		$output.='    <div class="wrapper" id="widget-wrapper-'.$persistedWidget->getId().'">'.PHP_EOL;
-		$output.='        <div class="widgetmenubar" id="widget-menubar-'.$persistedWidget->getId().'">'.PHP_EOL;
-		$output.='            <div class="left" style="height:16px;width:16px;"><a href="#" class="min">&nbsp;</a><!--<img src="icons/arrow-stop-090.png"/>--></div>'.PHP_EOL;
-		$output.='            <div class="right" style="height:16px;width:16px;" id="contextmenu-'.$persistedWidget->getId().'"><img src="/course/format/mashup/images/calendar.png"/></div>'.PHP_EOL;
-		$output.='            <div class="center" style="text-align:center;"><h2>'.$persistedWidget->getTitle() .'</h2></div>'.PHP_EOL;
-		$output.='        </div>'.PHP_EOL;
-		$output.='        <div class="widgetwrapper">'.PHP_EOL;
-		$output.='            <iframe class="vis" src="'.$widgetInstance->getUrl().'"></iframe>'.PHP_EOL;
-		$output.='        </div>'.PHP_EOL;
-		$output.='    </div>'.PHP_EOL;
-		$output.='</li>'.PHP_EOL;
-		return $output;
+		return $widgetInstance;
 	}
 
 	function getWidget($connection, $widgetIdentifier){
 		//global $USER, $COURSE, $CFG;
 		$widget = $connection->getOrCreateInstance ($widgetIdentifier);
-		if ( $widget ) {
-			/*
-			 $this->config->widget_url =$widget->getURL();
-			$this->config->widget_height = $widget->getHeight();
-			$this->config->widget_width = $widget->getWidth();
-			$this->config->widget_title = $widget->getTitle();
-			$this->title = $this->config->widget_title;
-			$this->instance_config_commit();
-			$this->refresh_content();
-			*/
-		}
 		return $widget;
 	}
 
