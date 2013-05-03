77367c0e4a97a11d7f4c6396c4153e44529ea775
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:06:57 +0100

    Updated to use json rather than return html markup

--- classes/actions/newInstanceAction.php
+++ classes/actions/newInstanceAction.php
@@ -5,20 +5,26 @@ $id = required_param('courseId', PARAM_INT);
 require_login($id);
 $PAGE->set_url('/course/format/mashup/classes/actions/newInstanceAction.php', array('id' => $id));
 
-
-//TODO add in the has_capability() check so only course_creator/teacher can do this
-
-if (isset($_GET['url']) && isset($_GET['title']) && isset($_GET['widgetType']) && isset($_GET['courseId'])  && isset($_GET['pageId']) ){
-
-	$layout="";
-	$url = $_GET['url'];
-	$title = $_GET['title'];
-	$widgetType = $_GET['widgetType'];
-	$courseId = $_GET['courseId'];
-	$pageId = $_GET['pageId'];
-	$moodleWidget = new MoodleWidget(null, $courseId, $url, $title, $widgetType, 1, 1, 1, 1, $pageId);
-	$instanceGenerator = new InstanceGenerator($moodleWidget->serialize());
-	$layout.=$instanceGenerator->getInstanceMarkup($moodleWidget);
-	echo $layout;
-}		
+// set permissions to prevent students etc to execute this
+$course = $PAGE->course;
+$context = get_context_instance(CONTEXT_COURSE, $course->id);
+if (!has_capability('moodle/course:manageactivities', $context)) {
+	echo "You do not have permission to complete this action";
+}
+else{
+	if (isset($_GET['url']) && isset($_GET['title']) && isset($_GET['widgetType']) && isset($_GET['courseId'])  && isset($_GET['pageId']) ){	
+		$json = array();
+		$url = $_GET['url'];
+		$title = $_GET['title'];
+		$widgetType = $_GET['widgetType'];
+		$courseId = $_GET['courseId'];
+		$pageId = $_GET['pageId'];
+		$moodleWidget = new MoodleWidget(null, $courseId, $url, $title, $widgetType, 1, 1, 1, 1, $pageId);
+		$instanceGenerator = new InstanceGenerator($moodleWidget->serialize());
+		$instance = $instanceGenerator->getWidgetInstance($moodleWidget);
+		$moodleWidget->setUrl($instance->getUrl());
+		array_push($json, $moodleWidget->toJson());
+		echo json_encode($json);
+	}
+}	
 ?>
