abc1d4bd3bcc1f44ae9097a91efea5776639b157
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:08:37 +0100

    removed comment

--- classes/actions/getPagesAction.php
+++ classes/actions/getPagesAction.php
@@ -5,7 +5,6 @@ require_once("{$CFG->dirroot}/course/format/mashup/classes/InstanceGenerator.php
 $id = required_param('courseId', PARAM_INT);
 require_login($id);
 $PAGE->set_url('/course/format/mashup/classes/actions/getPagesAction.php', array('id' => $id));
-//TODO add in the has_capability() check so only course_creator/teacher can do this
 
 if (isset($_GET['courseId']) ){
 	$json = array();
