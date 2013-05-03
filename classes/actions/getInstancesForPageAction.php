a540dcf6b30829c37098edce0162b02c18f483ce
ps3com <p.sharples@bolton.ac.uk>
2013-05-03 13:07:48 +0100

    updated to use json as data format

--- classes/actions/getInstancesForPageAction.php
+++ classes/actions/getInstancesForPageAction.php
@@ -6,24 +6,21 @@ $id = required_param('courseId', PARAM_INT);
 require_login($id);
 $PAGE->set_url('/course/format/mashup/classes/actions/getInstancesForPageAction.php', array('id' => $id));
 
-//TODO add in the has_capability() check so only course_creator/teacher can do this
-
-if (isset($_GET['mashupPageId']) ){
-
-	$layout="";
-	$mashupPageId = $_GET['mashupPageId'];
-	$mashupDatabaseHelper = new MashupDatabaseHelper();	
-	$pageLayout = $mashupDatabaseHelper->getLayoutForPage($mashupPageId);
+if (isset($_GET['pageId']) ){
+	$json = array();	
+	$pageId = $_GET['pageId'];
+	$mashupDatabaseHelper = new MashupDatabaseHelper();
+	$pageLayout = $mashupDatabaseHelper->getLayoutForPage($pageId);
 	
-	$layout = '<div class="gridster" style="width:100%" data-mashup-cols="'.$pageLayout.'">'.PHP_EOL;
-	$layout.='<ul>'.PHP_EOL;
-	$persistedWidgets = $mashupDatabaseHelper->getWidgetsForPage($mashupPageId);
+	$layoutData = array('layout' => $pageLayout);	
+	array_push($json, $layoutData);
+	$persistedWidgets = $mashupDatabaseHelper->getWidgetsForPage($pageId);
 	foreach ($persistedWidgets as $persistedWidget){
 		$instanceGenerator = new InstanceGenerator($persistedWidget->getId());
-		$layout.=$instanceGenerator->getInstanceMarkup($persistedWidget);
-	}
-	$layout.='</ul>'.PHP_EOL;
-	$layout.='</div>'.PHP_EOL;
-	echo $layout;
+		$instance = $instanceGenerator->getWidgetInstance($persistedWidget);
+		$persistedWidget->setUrl($instance->getUrl());
+		array_push($json, $persistedWidget->toJson());
+	}	
+	echo json_encode($json);
 }		
 ?>
