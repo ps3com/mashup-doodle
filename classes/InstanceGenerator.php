2614c117b1c90cf71bd08a23a3546d1a1f0489ee
ps3com <p.sharples@bolton.ac.uk>
2013-05-13 19:12:26 +0100

    basic handler for shindig gadgets

--- classes/InstanceGenerator.php
+++ classes/InstanceGenerator.php
@@ -1,30 +1,38 @@
 <?php
-require_once("{$CFG->dirroot}/course/format/mashup/framework/Widgeta.php");
+require_once("{$CFG->dirroot}/course/format/mashup/classes/connectors/w3c/Widget.php");
+require_once("{$CFG->dirroot}/course/format/mashup/classes/connectors/w3c/WookieConnectorService.php");
+require_once("{$CFG->dirroot}/course/format/mashup/classes/connectors/opensocial/ShindigConnectorService.php");
 require_once("{$CFG->dirroot}/course/format/mashup/classes/MoodleWidget.php");
-require_once("{$CFG->dirroot}/course/format/mashup/framework/WookieConnectorService.php");
 
 class InstanceGenerator {
 
-	private $connection = null;
+	private $wookieconnection = null;
+	private $shindigconnection = null;
 	private $instanceId = null;
 
 	function __construct($instanceId) {
 		global $USER,$CFG;
 		$this->instanceId = $instanceId;
-		$this->connection = new WookieConnectorService ($CFG->mashup_wookie_url, $CFG->mashup_wookie_key, $this->instanceId, $USER->id, $USER->username);
+		$this->wookieconnection = new WookieConnectorService ($CFG->mashup_wookie_url, $CFG->mashup_wookie_key, $this->instanceId, $USER->id, $USER->username);
+		$this->shindigconnection = new ShindigConnectorService($CFG->mashup_shindig_url);
 	}
 	
 	public function getWidgetInstance($persistedWidget){
-		global $USER;
-		$widgetInstance = $this->getWidget($this->connection, $persistedWidget->getUrl());	
-		//set username
-		$this->setProperty($widgetInstance, "username", $USER->username, $this->connection);
-		return $widgetInstance;
+		global $USER, $CFG;
+		if($persistedWidget->getWidgetType() == 1){
+			$widgetInstance = $this->getWidget($this->wookieconnection, $persistedWidget->getUrl());	
+			//set username
+			$this->setProperty($widgetInstance, "username", $USER->username, $this->wookieconnection);
+			return $widgetInstance;
+		}
+		else{
+			return $this->shindigconnection->getInstance($persistedWidget);
+		}
 	}
 
-	function getWidget($connection, $widgetIdentifier){
+	function getWidget($wookieconnection, $widgetIdentifier){
 		//global $USER, $COURSE, $CFG;
-		$widget = $connection->getOrCreateInstance ($widgetIdentifier);
+		$widget = $wookieconnection->getOrCreateInstance ($widgetIdentifier);
 		return $widget;
 	}
 
@@ -33,11 +41,11 @@ class InstanceGenerator {
 	 * NOTE that this should in future call the REST API using POST
 	 * but this is currently tricky with Moodle's download_file_content function
 	 */
-	function setProperty($widgetInstance, $key,$value, $connection){
+	function setProperty($widgetInstance, $key,$value, $wookieconnection){
 
 		global $USER, $COURSE,$CFG;
 		$newProperty = new Property ( $key, $value );
-		$r = $connection->setProperty ( $widgetInstance, $newProperty );
+		$r = $wookieconnection->setProperty ( $widgetInstance, $newProperty );
 	}
 
 	/**
@@ -45,7 +53,7 @@ class InstanceGenerator {
 	 * NOTE that this should in future call the REST API using POST
 	 * but this is currently tricky with Moodle's download_file_content function
 	 */
-	function addParticipant($widgetinstance, $connection){
+	function addParticipant($widgetinstance, $wookieconnection){
 
 		global $USER, $COURSE, $CFG;
 
@@ -55,7 +63,7 @@ class InstanceGenerator {
 			$src = $CFG->httpswwwroot.'/pix/u/f1.png';
 		}
 		$thisUser = new User ( $USER->id, $USER->username, $src );
-		$connection->addParticipant($widgetinstance, $thisUser );
+		$wookieconnection->addParticipant($widgetinstance, $thisUser );
 	}
 }
 
