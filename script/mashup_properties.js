968e93f22a2791060a9b87ce22827d42040dfbb2
ps3com <p.sharples@bolton.ac.uk>
2013-05-20 14:39:04 +0100

    fixed case issue in filename

--- script/mashup_properties.js
+++ script/mashup_properties.js
@@ -2,7 +2,7 @@ var mashup_properties = {
 		htmlParent: "mashup-content", // the html object to add the mashup to
 		courseId: null,			   // courseId - you will have to set this before calling MashupEngine.init()
 		importOMDLPageUrl: "/course/format/mashup/classes/actions/importOmdlAction.php",
-		exportOMDLPageUrl: "/course/format/mashup/classes/actions/exportOMDLAction.php",
+		exportOMDLPageUrl: "/course/format/mashup/classes/actions/exportOmdlAction.php",
 		getWidgetsForPageUrl: "/course/format/mashup/classes/actions/getInstancesForPageAction.php",
 		addNewPageUrl: "/course/format/mashup/classes/actions/newPageAction.php",
 		getPagesUrl: "/course/format/mashup/classes/actions/getPagesAction.php",
