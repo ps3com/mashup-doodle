af606cf58eda981290e325dac3c649bd5ae99ffa
ps3com <p.sharples@bolton.ac.uk>
2013-06-11 16:20:02 +0100

    cleaned up & added shindig API cal

--- includes/js_includes.php
+++ includes/js_includes.php
@@ -1,23 +1,15 @@
 <?php
 echo PHP_EOL;
 echo '<script type="text/javascript" src="/course/format/mashup/script/gridster/jquery.js"></script>'.PHP_EOL;
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery-ui-1.9.2.custom.min.js"></script>'.PHP_EOL;
-
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.core.js"></script>'.PHP_EOL;
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.widget.js"></script>'.PHP_EOL;
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.position.js"></script>'.PHP_EOL;
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.button.js"></script>'.PHP_EOL;
-echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.menu.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery-ui-1.10.3.custom.js"></script>'.PHP_EOL;
 echo '<script type="text/javascript" src="/course/format/mashup/script/jqueryUI/jquery.ui.menubar.js"></script>'.PHP_EOL;
-
 echo '<script type="text/javascript" src="/course/format/mashup/script/gridster/jquery.gridster-forked.js"></script>'.PHP_EOL;
-
 echo '<script type="text/javascript" src="/course/format/mashup/script/contextmenu/jquery.contextMenu.js"></script>'.PHP_EOL;
-
 echo '<script type="text/javascript" src="/course/format/mashup/script/json/json2.js"></script>'.PHP_EOL;
-
 echo '<script type="text/javascript" src="/course/format/mashup/script/mashup_properties.js"></script>'.PHP_EOL;
 echo '<script type="text/javascript" src="/course/format/mashup/script/mashupengine.js"></script>'.PHP_EOL;
-
-//echo '<script type="text/javascript" src="'.$CFG->mashup_shindig_url.'gadgets/js/shindig-container:rpc.js?c=1&debug=1"></script>';
+echo '<script type="text/javascript" src="'.$CFG->mashup_shindig_url.'gadgets/js/container:open-views:opensearch:rpc:xmlutil:pubsub-2.js?c=1&debug=1&container=default"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/rave/rave.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/rave/rave_opensocial.js"></script>'.PHP_EOL;
+echo '<script type="text/javascript" src="/course/format/mashup/script/rave/rave_wookie.js"></script>'.PHP_EOL;
 ?>
