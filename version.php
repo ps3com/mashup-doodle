54d4690670443ff81242425ffa2a3c995946709e
ps3com <p.sharples@bolton.ac.uk>
2013-06-11 19:03:31 +0100

    incremented moodle version

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013051403;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2013061104;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012112900;        // Requires this Moodle version.
 $plugin->component = 'format_mashup';    // Full name of the plugin (used for diagnostics).
